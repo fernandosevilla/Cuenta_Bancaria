3b25e21cbb4545dcb37589b3d7f7b4610a5152aa
fernandosevilla <fernandosevilla028@gmail.com>
2024-02-20 12:50:08 +0100

    Falta terminar de tratar excepciones

--- src/cuenta_bancaria/Cuenta.java
+++ src/cuenta_bancaria/Cuenta.java
@@ -4,11 +4,24 @@
  */
 package cuenta_bancaria;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 /**
- *
  * @author Fer
- */
+*/
+
 public interface Cuenta {
     static final double TIPO_RETENCION = 0.19;
-    double calcularInteres(double saldo, double tipo, fecha fechaHasta);
+
+    static double calcularInteres(double saldo, double tipo, LocalDate fechaDesde, LocalDate fechaHasta) {
+        long diferenciaDias = ChronoUnit.DAYS.between(fechaDesde, fechaHasta);
+        double beneficio = (saldo * tipo * diferenciaDias) / (100 * 365);
+        
+        if (beneficio > 0) {
+            beneficio *= 1 - TIPO_RETENCION;
+        }
+        
+        return beneficio;
+    }
 }
--- src/cuenta_bancaria/Cuenta_Bancaria.java
+++ src/cuenta_bancaria/Cuenta_Bancaria.java
@@ -4,62 +4,31 @@
  */
 package cuenta_bancaria;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.time.temporal.ChronoUnit;
-import java.util.Scanner;
 
 /**
  * @author Fer
  */
 
-public class Cuenta_Bancaria {
-    public static LocalDate leerFechaPorTeclado() {
-        Scanner teclado = new Scanner(System.in);
-        System.out.print("Ingrese una fecha en formato dd/mm/aaaa: ");
-        String fechaIngresada = teclado.nextLine();
-
-        // se crea el patrón con el formato de la fecha
-        DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-        try {
-            // conversión de String a LocalDate con el formato previamente creado
-            LocalDate fecha = LocalDate.parse(fechaIngresada, formatoFecha);
-
-            // Si la conversión no ha funcionado la fecha contiene null
-            if (esFechaValida(fecha)) {
-                return fecha;
-            } else {
-                System.out.println("Fecha no válida.");
-            }
-        } catch (DateTimeParseException e) {
-            System.out.println("Formato de fecha incorrecto. Se debe introducir en formato dd/mm/aaaa.");
-        }
-
-        return null;
-    }
-    
-    // Método para validar si una fecha es válida
-    public static boolean esFechaValida(LocalDate fecha) {
-        // Puedes agregar reglas adicionales de validación aquí si es necesario
-        return fecha != null;
-    }
+public class Cuenta_Bancaria implements Cuenta {   
+    double saldo;
+    double tipo;
+    LocalDate fechaDesde;
+    LocalDate fechaHasta;
+    double beneficio;
+    double ganancias;
     
     public static void main(String[] args) {
-        // Definir dos fechas de la clase LocalDate
-        LocalDate fechaInicio=null, fechaFin=null;
-        do{
-            System.out.print("Introducir la fecha inicial del periodo :");
-            fechaInicio = leerFechaPorTeclado();
-        }while(fechaInicio==null);
-        do{
-            System.out.print("Introducir la fecha final del periodo :");
-            fechaFin = leerFechaPorTeclado();
-        }while(fechaFin==null);
-        // Calcular los días transcurridos entre las dos fechas
-        long diasTranscurridos = ChronoUnit.DAYS.between(fechaInicio, fechaFin);
-
-        // Imprimir el resultado
-        System.out.println("Días transcurridos entre " + fechaInicio + " y " + fechaFin + ": " + diasTranscurridos + " días.");
+        Cuenta_Bancaria cuenta1 = new Cuenta_Bancaria();
+        cuenta1.saldo = 3000;
+        
+        cuenta1.tipo = 0.75;
+        System.out.print("Escribe la fecha de inicio: ");
+        cuenta1.fechaDesde = Leer_Fecha.leerFechaPorTeclado();
+        System.out.print("Escribe la fecha fin: ");
+        cuenta1.fechaHasta = Leer_Fecha.leerFechaPorTeclado();
+        cuenta1.beneficio = Cuenta.calcularInteres(cuenta1.saldo, cuenta1.tipo, cuenta1.fechaDesde, cuenta1.fechaHasta);
+        cuenta1.ganancias += cuenta1.beneficio;
+        cuenta1.saldo += cuenta1.ganancias;
+        System.out.println("El saldo tras las ganancias son: " + cuenta1.saldo);
     }
 }
